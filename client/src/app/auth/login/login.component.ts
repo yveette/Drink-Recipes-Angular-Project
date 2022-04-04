c1b27d02761550c2cf7b1d324672f9d7b00f1d0b
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-04-04 23:27:33 +0300

    Add login fields validation and custom email validator.

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/core/user.service';
+import { emailValidator } from '../util';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  errorMessage: string = '';
+
+  loginFormGroup: FormGroup = this.formBuilder.group({
+    // TODO: validate email
+    'email': new FormControl('', [Validators.required, emailValidator]),
+    'password': new FormControl('', [Validators.required, Validators.minLength(5)])
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  handleLogin(): void {
+    this.errorMessage = '';
+    // console.log(this.loginFormGroup.value);
+    
   }
 
 }
